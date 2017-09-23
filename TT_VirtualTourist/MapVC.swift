890635e2e0112bf9139e0b037a84d43344b559fa
TigranTT <tigrantt@yahoo.com>
2017-09-22 23:44:56 -0500

    creating

---
+++ TT_VirtualTourist/MapVC.swift
@@ -0,0 +1,15 @@
+//
+//  MapVC.swift
+//  TT_VirtualTourist
+//
+//  Created by Tigran Tshorokhyan on 9/22/17.
+//  Copyright © 2017 Tigran Tshorokhyan. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+
+class MapVC: UIViewController {
+    
+}
