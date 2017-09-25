c14512415a89df3e3a19641a225c67173727c2d0
TigranTT <tigrantt@yahoo.com>
2017-09-24 23:00:38 -0500

    update

---
+++ TT_VirtualTourist/GalleryVC.swift
@@ -0,0 +1,43 @@
+//
+//  GalleryVC.swift
+//  TT_VirtualTourist
+//
+//  Created by Tigran Tshorokhyan on 9/24/17.
+//  Copyright © 2017 Tigran Tshorokhyan. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import MapKit
+
+class GalleryVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var pin: Pin!
+    
+    
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
