86ecc246e47e529381909c184bef7d74e2001701
TigranTT <tigrantt@yahoo.com>
2017-10-04 23:16:59 -0500

    pinch zoom and Share functions

--- TT_VirtualTourist/PhotoVC.swift
+++ TT_VirtualTourist/PhotoVC.swift
@@ -9,14 +9,19 @@
 import Foundation
 import UIKit
 
-class PhotoVC: UIViewController {
+class PhotoVC: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var photo: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.isUserInteractionEnabled = true
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 6.0
+        scrollView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -25,13 +30,34 @@ class PhotoVC: UIViewController {
             imageView.image = image
         }
     }
+    
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 
     
+    func generateMemedImage() -> UIImage {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let selectedPhoto:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return selectedPhoto
+    }
     
     
-    
-    
-    
+    @IBAction func share(_ sender: Any) {
+        let memedImage = generateMemedImage()
+        let activityView = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        activityView.completionWithItemsHandler = {
+            (_, successful, _, _) in
+            
+            if successful{
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+        present(activityView, animated: true, completion: nil)
+    }
     
     
 }
